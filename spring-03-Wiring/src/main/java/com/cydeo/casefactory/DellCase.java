9d2b3cf84d3c2e99e901bacbf50bdb0df0954d0b
LENOVO <osmnozdmrosh@gmail.com>
2023-08-02 07:15:24 +0300

    Wiring

---
+++ spring-03-Wiring/src/main/java/com/cydeo/casefactory/DellCase.java
@@ -0,0 +1,18 @@
+package com.cydeo.casefactory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DellCase extends Case {
+
+    public DellCase(String model, String manufacturer, String powerSupply,Dimensions dimensions) {
+        super(model, manufacturer, powerSupply, dimensions);
+    }
+
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
