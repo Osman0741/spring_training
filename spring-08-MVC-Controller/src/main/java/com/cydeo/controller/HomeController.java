56bd2c156d5f35ed9cc32f59e81d9ed896ca4503
LENOVO <osmnozdmrosh@gmail.com>
2023-08-09 11:49:41 +0300

    Spring MVC

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home(){
+        return "home.html";
+    }
+    @RequestMapping("/osman")
+    public String home2(){
+        return "home.html";
+    }
+    @RequestMapping // nothing and /
+    public String home3(){
+        return "home.html";
+    }
+    @RequestMapping ({"/apple","/orange"})
+    public String home4(){
+        return "home.html";
+    }
+
+}
