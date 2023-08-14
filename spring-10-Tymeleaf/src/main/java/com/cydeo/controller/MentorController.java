8f66518dbffef7cec3c708bcda14289172353c68
LENOVO <osmnozdmrosh@gmail.com>
2023-08-14 15:45:27 +0300

    Spring  Thymeleaf

---
+++ spring-10-Tymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,26 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/register")
+    public String register(){
+
+        return "student/register";
+    }
+    @RequestMapping("/drop")
+    public String drop(){
+
+
+
+        return "student/register";
+
+    }
+
+}
---
+++ spring-10-Tymeleaf/src/test/java/com/cydeo/spring10tymeleaf/Spring10TymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring10tymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10TymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
