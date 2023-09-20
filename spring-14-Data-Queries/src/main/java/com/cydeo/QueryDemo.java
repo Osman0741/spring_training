8856c31911e20a8c619b726eb1ef9ab499ac9d73
LENOVO <osmnozdmrosh@gmail.com>
2023-09-20 16:36:22 +0300

    Spring Data Queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,17 +9,28 @@ import org.springframework.stereotype.Component;
 public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("findByCountry: "+regionRepository.findByCountry("Canada"));
+        System.out.println("----Regions-------");
 
+        System.out.println("findByCountry: "+regionRepository.findByCountry("Canada"));
+        System.out.println("findByCountry: "+regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainsOrderByRegionDesc: "+regionRepository.findByCountryContainsOrderByRegionDesc("United"));
+        System.out.println(" findTopByCountry"+regionRepository.findTopByCountry("United States"));
+        System.out.println(" findTopByCountry"+regionRepository.findTopByCountryContainsOrderByRegion("United States"));
+
+        System.out.println("----Departments-------");
+        System.out.println("findByDepartment "+departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision "+departmentRepository.findByDivision("Health"));
 
 
     }
--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -9,6 +9,18 @@ public interface RegionRepository extends JpaRepository<Region,Integer> {
 
     //Display all regions in Canada
     List<Region> findByCountry(String country);
+    List<Region> getByCountry(String country);
+
+    //Display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
+    //Display all regions with country name includes 'United' in order(region)
+    List<Region> findByCountryContainsOrderByRegionDesc(String country);
+
+    //Display top 2 regions in United States
+    List<Region> findTopByCountry(String country);
+
+    List<Region> findTopByCountryContainsOrderByRegion(String country);
 
 
 
