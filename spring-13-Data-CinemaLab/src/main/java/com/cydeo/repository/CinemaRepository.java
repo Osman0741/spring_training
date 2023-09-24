37cee559c137ad46b13682427015d368e16e2cc1
LENOVO <osmnozdmrosh@gmail.com>
2023-09-24 17:44:06 +0300

    Spring Data Cinema Lab Queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -4,10 +4,11 @@ import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface CinemaRepository extends JpaRepository <Cinema,Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -4,9 +4,10 @@ import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     // ------------------- JPQL QUERIES ------------------- //
