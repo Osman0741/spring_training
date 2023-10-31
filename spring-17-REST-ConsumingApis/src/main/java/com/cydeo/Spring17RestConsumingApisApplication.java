294c90f7e2fcf51117b297f5fdd79985b6aa7869
LENOVO <osmnozdmrosh@gmail.com>
2023-10-31 23:15:42 +0300

    Spring REST  Consuming Apis done by using Feign Client

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,11 +1,14 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Spring17RestConsumingApisApplication {
 
     public static void main(String[] args) {
@@ -14,5 +17,9 @@ public class Spring17RestConsumingApisApplication {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
     }
 }
