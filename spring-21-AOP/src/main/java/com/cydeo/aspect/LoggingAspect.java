17051b9e6e28f41416b2db6cc0577aadd5e9d815
LENOVO <osmnozdmrosh@gmail.com>
2023-12-02 15:11:17 +0300

    Spring AOP Logging Aspect

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.cydeo.aspect;
 
 import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,59 +38,82 @@ public class LoggingAspect {
 //        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
 //              ,joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
-
-    @Pointcut("within(com.cydeo.controller..*)")
-    public void anyControllerOperation() {
-    }
-
-    @Pointcut("@within(org.springframework.stereotype.Service)")
-    public void anyServiceOperation() {
-    }
-
-    @Before("anyControllerOperation() || anyServiceOperation()")
-    public void beforeControllerOrServiceAdvice(JoinPoint joinPoint) {
-        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
-                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
-    }
-
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
-    public void anyDeleteControllerOperation() {
-    }
-
-    @Before("anyDeleteControllerOperation()")
-    public void beforeDeleteMappingAnnotation(JoinPoint joinPoint) {
-        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
-                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
-    }
+//
+//    @Pointcut("within(com.cydeo.controller..*)")
+//    public void anyControllerOperation() {
+//    }
+//
+//    @Pointcut("@within(org.springframework.stereotype.Service)")
+//    public void anyServiceOperation() {
+//    }
+//
+//    @Before("anyControllerOperation() || anyServiceOperation()")
+//    public void beforeControllerOrServiceAdvice(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+//
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+//    public void anyDeleteControllerOperation() {
+//    }
+//
+//    @Before("anyDeleteControllerOperation()")
+//    public void beforeDeleteMappingAnnotation(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+//
+//    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+//    public void loggingAnnotationPC() {
+//    }
+//
+//    @Before("loggingAnnotationPC()")
+//    public void beforeLoggingAnnotation(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+//
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+//    public void afterReturningGetMappingAnnotation() {
+//    }
+//
+//    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "result")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, Object result) {
+//        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), result.toString());
+//    }
+//
+//    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "results")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results) {
+//        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), results.toString());
+//    }
+//    // CourseDTO -> Object     --> This is ok
+//    // List<CourseDTO> -> List<Object>     --> This is not ok
+//    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, RuntimeException exception) {
+//        logger.error("After Throwing -> Method: {}, Result: {}", joinPoint.getSignature().toShortString(), exception.getMessage());
+//    }
 
     @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
     public void loggingAnnotationPC() {
     }
 
-    @Before("loggingAnnotationPC()")
-    public void beforeLoggingAnnotation(JoinPoint joinPoint) {
-        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
-                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
-    }
+    @Around("loggingAnnotationPC()")
+    public Object anyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJoinPoint) {
+        logger.info("Before -> Method: {} - Parameter {}",
+                proceedingJoinPoint.getSignature().toShortString(), proceedingJoinPoint.getArgs());
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void afterReturningGetMappingAnnotation() {
-    }
+        Object result = null;
 
-    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "result")
-    public void afterReturningGetMappingOperation(JoinPoint joinPoint, Object result) {
-        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), result.toString());
-    }
+        try {
+            result = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
 
-    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "results")
-    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results) {
-        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), results.toString());
-    }
-    // CourseDTO -> Object     --> This is ok
-    // List<CourseDTO> -> List<Object>     --> This is not ok
-    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
-    public void afterReturningGetMappingOperation(JoinPoint joinPoint, RuntimeException exception) {
-        logger.error("After Throwing -> Method: {}, Result: {}", joinPoint.getSignature().toShortString(), exception.getMessage());
+        logger.info("After -> Method:{} - Result: {}",
+                proceedingJoinPoint.getSignature().toShortString(), result.toString());
+
+        return result;
     }
 
 
